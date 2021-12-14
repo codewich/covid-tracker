d711d393cf7412ffcdb3763890553ce897150d11
Michael Lin <linzhiming111@gmail.com>
2021-12-14 01:18:35 -0500

    Add model to save stats

---
+++ src/main/java/com/example/covidtracker/models/RegionalStat.java
@@ -0,0 +1,41 @@
+package com.example.covidtracker.models;
+
+public class RegionalStat {
+
+    private String provinceOrState;
+    private String countryOrRegion;
+    private int latestTotalCases;
+
+    public String getProvinceOrState() {
+        return provinceOrState;
+    }
+
+    public void setProvinceOrState(String provinceOrState) {
+        this.provinceOrState = provinceOrState;
+    }
+
+    public String getCountryOrRegion() {
+        return countryOrRegion;
+    }
+
+    public void setCountryOrRegion(String countryOrRegion) {
+        this.countryOrRegion = countryOrRegion;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "RegionalStat{" +
+                "provinceOrState='" + provinceOrState + '\'' +
+                ", countryOrRegion='" + countryOrRegion + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
+}
--- src/main/java/com/example/covidtracker/services/CovidDataService.java
+++ src/main/java/com/example/covidtracker/services/CovidDataService.java
@@ -1,5 +1,6 @@
 package com.example.covidtracker.services;
 
+import com.example.covidtracker.models.RegionalStat;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,15 +13,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CovidDataService {
 
     private  static String COVID_DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private List<RegionalStat> allStats = new ArrayList<>();
 
     @PostConstruct
-    @Scheduled(cron = "* * 1 * * *")
+    @Scheduled(cron = "0 0 * * * ?")
     public void fetchCovidData() throws IOException, InterruptedException {
+        List<RegionalStat> newStats = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(COVID_DATA_URL))
@@ -29,9 +34,13 @@ public class CovidDataService {
         StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
-            String provinceOrState = record.get("Province/State");
-            String countryOrRegion = record.get("Country/Region");
+            RegionalStat regionalStat = new RegionalStat();
+            regionalStat.setProvinceOrState(record.get("Province/State"));
+            regionalStat.setCountryOrRegion(record.get("Country/Region"));
+            regionalStat.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
+            newStats.add(regionalStat);
         }
+        this.allStats = newStats;
     }
 
 }
